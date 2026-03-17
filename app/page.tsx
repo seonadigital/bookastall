7206af6e5b6c31da928e975e383b5df98ea6c8a7
Seona Digital Solutions <giriddhar@gmail.com>
2026-03-17 18:11:42 +0530

    remove app

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,6 @@ export default function HomePage() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <AppBanner />
       <main className="flex-1">
         <HeroSection />
         <CityGrid />
